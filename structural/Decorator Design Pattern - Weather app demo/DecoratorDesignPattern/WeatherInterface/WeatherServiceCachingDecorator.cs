ddf84ace15ad72e0f5c5284bd5eb2dbcecee6043
alina-olaru <olarualina01@gmail.com>
2021-01-02 16:18:14 +0200

    Decorator dp

---
+++ structural/Decorator Design Pattern - Weather app demo/DecoratorDesignPattern/WeatherInterface/WeatherServiceCachingDecorator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecoratorDesignPattern.WeatherInterface
+{
+    public class WeatherServiceCachingDecorator : IWeatherService
+    {
+        private IWeatherService _weatherService;
+        private IMemoryCache _cache;
+
+        public WeatherServiceCachingDecorator(IWeatherService weatherService, IMemoryCache cache)
+        {
+            _weatherService = weatherService;
+            _cache = cache;
+        }
+
+        public CurrentWeather GetCurrentWeather(string location)
+        {
+            String cacheKey = $"WeatherConditions::{location}";
+            if (_cache.TryGetValue<CurrentWeather>(cacheKey, out var currentWeather))
+            {
+                return currentWeather;
+            }
+            else
+            {
+                var currentCondition = _weatherService.GetCurrentWeather(location);
+                _cache.Set<CurrentWeather>(cacheKey, currentCondition, TimeSpan.FromMinutes(30));
+                return currentCondition;
+            }
+
+          
+        }
+
+        public LocationForecast GetForecast(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ structural/Decorator Design Pattern - Weather app demo/DecoratorDesignPattern/WeatherInterface/WeatherServiceLoggingDecorator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecoratorDesignPattern.WeatherInterface
+{
+    // decorator must implement the same interface as the class that will be decorating
+    // every method from that interface must have at least a default/ basic implementation
+    public class WeatherServiceLoggingDecorator : IWeatherService
+    {
+        private IWeatherService _weatherService;
+        private ILogger<WeatherServiceLoggingDecorator> _logger;
+
+        public WeatherServiceLoggingDecorator(IWeatherService weatherService, ILogger<WeatherServiceLoggingDecorator> logger)
+        {
+            _weatherService = weatherService;
+            _logger = logger;
+        }
+
+        public CurrentWeather GetCurrentWeather(string location)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            CurrentWeather currentWeather = _weatherService.GetCurrentWeather(location);
+            stopwatch.Stop();
+            long elapsedMillis = stopwatch.ElapsedMilliseconds;
+            _logger.LogWarning("Retrieved data for {location} in {@elapsedMillis} ms");
+            return currentWeather;
+        }
+
+        public LocationForecast GetForecast(string location)
+        {
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            LocationForecast locationForecast = _weatherService.GetForecast(location);
+            stopwatch.Stop();
+            long elapsedMillis = stopwatch.ElapsedMilliseconds;
+            _logger.LogWarning("Retrieved location Forest for {location} in {@elapsedMillis} ms");
+            return locationForecast;
+
+
+        }
+    }
+}
